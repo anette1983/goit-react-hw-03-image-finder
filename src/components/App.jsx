2fca6c8d57f98b93a74c0051c77f893cf172c51e
Hanna <anna_kon@bigmir.net>
2023-03-22 01:04:00 +0200

    hook version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -132,3 +132,95 @@ export class App extends Component {
     );
   }
 }
+
+
+/*import { useState, useEffect } from 'react';
+import Loader from 'components/Loader/Loader';
+import * as API from 'services/api';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Modal from './Modal/Modal';
+import Button from './Button/Button';
+
+function App() {
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [largeImage, setLargeImage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isShownModal, setIsShownModal] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [imageAlt, setImageAlt] = useState('');
+
+  useEffect(() => {
+    getPhotos(searchQuery, page);
+  }, [searchQuery, page]);
+
+  async function getPhotos(query, page) {
+    setIsLoading(true);
+
+    try {
+      const response = await API.fetchImages(query, page);
+
+      if (response.totalHits < 12 || response.hits.length < 12) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+
+      if (response.totalHits === 0) {
+        alert(`There are no photos for the "${searchQuery}" query`);
+      }
+
+      setPhotos(prevPhotos => [...prevPhotos, ...response.hits]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function handleSearchSubmit(query) {
+    if (searchQuery === query) {
+      return;
+    }
+
+    setSearchQuery(query);
+    setIsVisible(false);
+    setPhotos([]);
+    setPage(1);
+  }
+
+  function handleLoadMoreClick() {
+    setPage(prevPage => prevPage + 1);
+    setIsLoading(true);
+  }
+
+  function toggleModal(image, imageAlt) {
+    setIsShownModal(prevIsShownModal => !prevIsShownModal);
+    setLargeImage(image);
+    setImageAlt(imageAlt);
+  }
+
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleSearchSubmit} />
+      {error && <p>Something went wrong :( Try again later!</p>}
+      {photos.length > 0 && (
+        <ImageGallery
+          photos={photos}
+          toggleModal={toggleModal}
+          onLoadMoreClick={handleLoadMoreClick}
+        />
+      )}
+      {isLoading && <Loader />}
+      {isVisible && !isLoading && <Button onClick={handleLoadMoreClick} />}
+      {isShownModal && (
+        <Modal image={largeImage} onClose={toggleModal} alt={imageAlt} />
+      )}
+    </div>
+  );
+}
+
+export default App;*/
